e5746b8b221f69904a0dbed0551e569d1b8396c0
Zo Beki <uobeki.40@gmail.com>
2021-01-30 23:58:41 -0500

    final push

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Weather-Dashboard
 ## Task :man_technologist:
+For this assignment, we were tasked with creating a Weather Dashboard application that changes dynamically. The application uses jQuery to make an AJAX call to the Open Weather Map API to retrieve information such as City coordinates, Temperature, Humidity, Wind Speed, and even icons representing current weather condtions. The information from that call is then appended to the HTML with jQery.
 
+Once a search has been made the user's recent searches populate in a <div> section underneathe the search bar.
 
-## Thoughts :thinking:
+![App Screenshot](/assets/Weather-Dashboard.png)
+
+## Thoughts and Notes :thinking:
+This assignment was challenging at first glance but after some psuedo-coding and application of prior knowledge, it became easier to deal with. My code isn't perfect though, there is likely a cleaner way to display the information from the API. Still, I'm proud of the this turned out.
+
+## APIs and Libraries
+This assignment made use of the Open Weather Map API, specifically the Five Weather Day Forecast API. 
+
+Documentation: https://openweathermap.org/forecast5
+
+The Moment JS library was utilized to retrieve the current date.
+
+Documentation: https://momentjs.com/docs/
+
+The Bootstrap CSS library was used to create responsive and styled elements on the page.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,109 @@
-// User types city name in the search box
-// var cityName = $(".search").val();
-// console.log($(".search").val());
-// When search is clicked, ajax request is made to pull weather info for that city is from API
-$("button").on("click", function(){
-    alert("stop poking me!")
-})
-    // Info for today's weather populates the card in the <main> section of body
+$(".searchBtn").on("click", function (event) {
+    event.preventDefault();
+    $(".weather-text").empty();
+    $("#forecast").empty();
+    $("#uvBtn").remove()
+
+    var city = $(".search").val();
+    console.log(city);
+
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2d325a02be6346b80e081d792e1e2cb4"
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (result) {
+        console.log(result);
+        // City Name and Date
+        var currentDate = moment().format('L');
+        var cityName = $(".city-name").text(result.city.name + " (" + currentDate + ")");
+
+        // Icon
+        var weatherIcon = $(".wicon");
+        var iconCode = result.list[0].weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+        console.log(iconUrl);
+        weatherIcon.attr("src", iconUrl);
+        $(".card-body").append(weatherIcon);
+
         // Temperature
+        var temperature = $("<p class='weather-text'>").text("Temperature: " + result.list[0].main.temp + "°F");
+        $(".card-body").append(temperature);
+
         // Humidity
+        var humidity = $("<p class='weather-text'>").text("Humidity: " + result.list[0].main.humidity + "%");
+        $(".card-body").append(humidity);
+
         // Wind Speed
+        var windSpeed = $("<p class='weather-text'>").text("Wind Speed: " + result.list[0].wind.speed + " MPH");
+        $(".card-body").append(windSpeed);
+
         // UV Index
-    // 5 Day Forecast populates with 5 cards showing the weather for the next 5 days 
-        // Included in the card is the date, weather image, temperature. humidity
-    // List with city name is created to the right of the page underneath search bar
-        // New cities prepend as more cities are searched for
+        var lat = result.city.coord.lat;
+        var lon = result.city.coord.lon;
+        var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=2d325a02be6346b80e081d792e1e2cb4";
+        console.log(uvUrl);
+
+        $.ajax({
+            url: uvUrl,
+            method: "GET"
+        }).then(function (response) {
+            var uvIndex = $("<button class='btn bg-warning weather-text' id='uvBtn'>").text("UV Index: " + response.value);
+            $(".card-body").append(uvIndex);
+
+        });
+
+        // Five-Day Forecast
+
+        var list = result.list;
+        console.log(list);
+
+        for (var i = 0; i < list.length; i += 8) {
+
+
+
+            var forecast = $("<div class='card bg-primary text-white' style='width: 10rem; margin: 8px'>");
+
+            var foreDate = $("<p class='card-title'>").text(result.list[i].dt_txt.slice(0, 10));
+            forecast.append(foreDate);
+
+
+
+            var foreTemp = $("<p class='card-text'>").text("Temperature: " + result.list[i].main.temp + "°F");
+            forecast.append(foreTemp);
+
+            var cardIcons = result.list[i].weather[0].icon;
+            var foreIcon = $("<img>");
+            var foreIconUrl = "http://openweathermap.org/img/w/" + cardIcons + ".png";
+            foreIcon.attr("src", foreIconUrl);
+            foreIcon.attr("style", "height: 40px; width: 40px");
+            forecast.append(foreIcon);
+
+            var foreHumid = $("<p class='card-text'>").text("Humidity: " + result.list[i].main.humidity + "%")
+            forecast.append(foreHumid);
+
+            $("#forecast").append(forecast);
+        };
+        // Recent Searches
+        var cityVal = city.trim();
+        var searchArr = [];
+        searchArr.push(cityVal);
+        console.log(searchArr);
+        localStorage.setItem('city', JSON.stringify(searchArr));
+
+        recentSearch();
+    })
+    function recentSearch() {
+        var searchHistory = JSON.parse(localStorage.getItem("city"));
+        var recentBtn = $("<button type='submit' class='btn btn-outline-primary recent-button' style='width: 12rem'>").text(searchHistory);
+        var historyDiv = $("<div>");
+        historyDiv.append(recentBtn);
+        $(".recent-search").prepend(recentBtn);
+    }
+    $(".recent-button").on('click', function (e) {
+        e.preventDefault();
+        console.log($(this).text());
+        city = $(this).text();
+        console.log(city);
+    })
+}) 
